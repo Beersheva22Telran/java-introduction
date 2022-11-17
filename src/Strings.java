0337e226b26e40632308c4ef7a5f44f809c70740
Yuragranovsky <yuragranovsky@gmail.com>
2022-11-17 17:12:27 +0200

    CW #9

---
+++ src/Strings.java
@@ -0,0 +1,17 @@
+
+public class Strings {
+/**
+ * 
+ * @param str1
+ * @param str2
+ * @return true if str2 is an anagram of str1
+ * complexity O[N] two passes
+ * only one additional array
+ * only symbols or English letters
+ * 1. the same length
+ * 2. the same symbols just in different order
+ */
+	public static boolean isAnagram(String str1, String str2) {
+	return false;
+}
+}
