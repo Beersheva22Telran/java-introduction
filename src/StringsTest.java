8865913b4b2c4749e690690ea0db0e380d64ddde
Yuragranovsky <yuragranovsky@gmail.com>
2022-11-22 16:57:09 +0200

    CW #10 Regular expressions first

---
+++ src/StringsTest.java
@@ -0,0 +1,49 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class StringsTest {
+String word="yellow";
+	@Test
+	//@Disabled
+	void sortStringsNumbersTest() {
+		String array[] = {"127", "-128", "0", "25", "25", "-128", "127"};
+		String expected[] = {"-128", "-128", "0", "25", "25", "127", "127"};
+		Strings.sortStringNumbers(array);
+		assertArrayEquals(expected, array);
+	}
+	@Test
+	//@Disabled
+	void isAnagramTest() {
+		
+		assertTrue(Strings.isAnagram(word, "loweyl")); 
+		assertTrue(Strings.isAnagram(word, "elolyw")); 
+		assertTrue(Strings.isAnagram(word, "wolley")); 
+		assertTrue(Strings.isAnagram(word, "loleyw")); 
+		assertTrue(Strings.isAnagram("yttttoooo", "ooootttty"));
+		assertFalse(Strings.isAnagram(word,"")); 
+		assertFalse(Strings.isAnagram(word, "yellob")); 
+		assertFalse(Strings.isAnagram(word,"yello")); 
+		assertFalse(Strings.isAnagram(word,"yelllo")) ;
+		assertFalse(Strings.isAnagram(word, "wolkez"));
+		assertFalse(Strings.isAnagram(word, "yeooow"));
+		assertFalse(Strings.isAnagram("yttttoooo", "yoootttty"));
+	}
+	@Test
+	void javaVariableTest() {
+		assertTrue("java".matches(Strings.javaNameExp()));
+		assertFalse("1java".matches(Strings.javaNameExp()));
+		assertFalse("_".matches(Strings.javaNameExp()));
+		assertTrue("__".matches(Strings.javaNameExp()));
+		assertTrue("java_1_2".matches(Strings.javaNameExp()));
+		assertTrue("$".matches(Strings.javaNameExp()));
+		assertFalse("$ _".matches(Strings.javaNameExp()));
+		
+		
+	}
+	
+	
+	
+
+}
